b0bddfa12aeeff6e68ed09f4320e8e3ec709a7e8
Valentin Fedoskin <valentin.fedoskin@gmail.com>
2020-03-23 17:02:45 +0100

    add env to releaser script

--- hacks/release-app.sh
+++ hacks/release-app.sh
@@ -6,11 +6,12 @@ set -o pipefail
 
 # script emulates a release process of new app by tagging a docker image with a random string and pushing it to a repo
 REPO="slamdev"
+ENV="prod"
 
 for i in Dockerfile-*; do
   img=$(echo "${i}" | cut -d'-' -f2)
   rnd=$(openssl rand -hex 3)
-  tag="${REPO}/${img}:${rnd}"
+  tag="${REPO}/${img}:${ENV}-${rnd}"
   docker build --file "${i}" --build-arg "RND=${rnd}" --tag "${tag}" .
   docker push "${tag}"
 done
